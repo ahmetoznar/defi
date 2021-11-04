ca2774086d77c7ceabae54db47e16c39d4b1eb71
Ahmet Oznar <45452358+ahmetoznar@users.noreply.github.com>
2021-11-05 00:12:49 +0300

    Add files via upload

---
+++ argonStakeMaster.sol
@@ -0,0 +1,79 @@
+/*
+   ___                    
+  / _ | _______ ____  ___ 
+ / __ |/ __/ _ `/ _ \/ _ \
+/_/ |_/_/  \_, /\___/_//_/
+          /___/        
+             
+*/
+
+pragma solidity 0.6.12;
+
+import "ArgonStakingPool.sol";
+
+contract ArgonStakeMaster is Ownable {
+    address[] public pools;
+    event PoolCreated(
+        address owner,
+        address newPoolAddress,
+        address stakingToken,
+        address poolToken,
+        uint256 startBlock,
+        uint256 finishBLock,
+        uint256 poolTokenAmount,
+        bool isPenalty,
+        uint256 penaltyRate,
+        address penaltyAddress,
+        uint256 penaltyBlockLength
+    );
+
+    function createStakingPool(
+        IERC20 _stakingToken,
+        IERC20 _poolToken,
+        uint256 _startBlock,
+        uint256 _finishBlock,
+        uint256 _poolTokenAmount,
+        bool _isPenalty,
+        uint256 _penaltyRate,
+        address _penaltyAddress,
+        uint256 _penaltyBlockLength
+    ) public onlyOwner {
+        ArgonStakingPool newStakingPool =
+            new ArgonStakingPool(
+                _stakingToken,
+                _poolToken,
+                _startBlock,
+                _finishBlock,
+                _poolTokenAmount,
+                _isPenalty,
+                _penaltyRate,
+                _penaltyAddress,
+                _penaltyBlockLength
+            );
+        newStakingPool.transferOwnership(msg.sender);
+        _poolToken.transferFrom(
+            msg.sender,
+            address(newStakingPool),
+            _poolTokenAmount
+        );
+        pools.push(address(newStakingPool));
+
+        emit PoolCreated(
+            msg.sender,
+            address(newStakingPool),
+            address(_stakingToken),
+            address(_poolToken),
+            _startBlock,
+            _finishBlock,
+            _poolTokenAmount,
+            _isPenalty,
+            _penaltyRate,
+            _penaltyAddress,
+            _penaltyBlockLength
+        );
+    }
+
+    function getAllPools() public view returns (address[] memory) {
+        return pools;
+    }
+}
